67e3708adc126498a2bf15452a628ba54b1dec38
tsacchet <tony_sacchetta@hotmail.com>
2019-02-13 15:26:21 +1100

    changes to batch file
    include git commands

--- build.bat
+++ build.bat
@@ -4,3 +4,10 @@ set /p sha=<sha.txt
 echo sha = %sha%
 
 go build -ldflags "-X main.version=1.0 -X main.sha=%sha%"
+
+
+git add GolangRESTAPI.exe
+git commit GolangRESTAPI.exe -m "change to exe"
+git push
+
+
