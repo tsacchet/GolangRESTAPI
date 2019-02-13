c02ae5c301502a4e8e5c665036a44ba07a9d3133
tsacchet <tony_sacchetta@hotmail.com>
2019-02-13 13:40:16 +1100

    Updated changes to build file
    changes include getting the sha from git. the second is version.

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-go build -ldflags "-X main.version=1.0 -X main.sha=abc57858585"
+git rev-parse HEAD > sha.txt
+set /p sha=<sha.txt
+
+echo sha = %sha%
+go build -ldflags "-X main.version=1.0 -X main.sha=%sha%"
